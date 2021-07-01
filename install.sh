54a195af5e88170ec7894a3b4c2dd6429356a4cd
zhao123gg <86518347+zhao123gg@users.noreply.github.com>
2021-07-01 16:24:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -372,13 +372,13 @@ EOF
     cat > /etc/xl2tpd/xl2tpd.conf<<EOF
 [global]
 port = 1701
-listen-addr = 192.168.0.205
+listen-addr = 192.168.0.230
 ipsec saref = yes
 auth file = /etc/ppp/chap-secrets
 
 [lns default]
 ip range = ${iprange}.2-${iprange}.254
-local ip = 192.168.0.205
+local ip = 192.168.0.230
 require chap = yes
 refuse pap = yes
 require authentication = yes
