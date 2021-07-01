5265b5acc4d60a1191cb7ba2f8fe18f8446eb967
zhao123gg <86518347+zhao123gg@users.noreply.github.com>
2021-07-01 21:57:03 +0800

    Update l2tp.sh

--- l2tp.sh
+++ l2tp.sh
@@ -372,13 +372,13 @@ EOF
     cat > /etc/xl2tpd/xl2tpd.conf<<EOF
 [global]
 port = 1701
-listen-addr = 192.168.0.7
+listen-addr = 192.168.0.9
 ipsec saref = yes
 auth file = /etc/ppp/chap-secrets
 
 [lns default]
 ip range = ${iprange}.2-${iprange}.254
-local ip = 192.168.0.7
+local ip = 192.168.0.9
 require chap = yes
 refuse pap = yes
 require authentication = yes
