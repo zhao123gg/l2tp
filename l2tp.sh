240f37ae45d75cb1c15df3c54e75d40d35c339a9
zhao123gg <86518347+zhao123gg@users.noreply.github.com>
2021-07-10 00:45:25 +0800

    Update l2tp.sh

--- l2tp.sh
+++ l2tp.sh
@@ -360,8 +360,8 @@ conn l2tp-psk-nonat
     ikelifetime=8h
     keylife=1h
     type=transport
-    left=47.242.93.84
-    leftid=47.242.93.84
+    left=43.128.12.120
+    leftid=43.128.12.120
     leftnexthop=%defaultroute
     leftprotoport=17/1701
     right=%any
@@ -381,13 +381,13 @@ EOF
     cat > /etc/xl2tpd/xl2tpd.conf<<EOF
 [global]
 port = 1701
-listen-addr = 192.168.0.114
+listen-addr = 192.168.0.17
 ipsec saref = yes
 auth file = /etc/ppp/chap-secrets
 
 [lns default]
 ip range = ${iprange}.2-${iprange}.254
-local ip = 192.168.0.114
+local ip = 192.168.0.17
 require chap = yes
 refuse pap = yes
 require authentication = yes
